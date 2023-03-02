96c3887d720edf3ae1124d1e2a79c710edfade4b
Juan Ramón Rueda <juanramonrueda.94@gmail.com>
2023-03-02 16:41:06 +0100

    Se crea el ejercicio con do-while

---
+++ src/2-bucles/dowhile-loop/ejercicio-01.php
@@ -0,0 +1,21 @@
+<?php
+  $i = 1;
+  do {
+    echo "<h2>Tabla de multiplicar del número $i</h2>";
+    
+    $j = 1;
+    echo "<table border='1'>";
+      while ($j <= 10) {
+        echo "<tr>";
+          echo "<td>$i</td>";
+          echo "<td>x</td>";
+          echo "<td>$j</td>";
+          echo "<td>=</td>";
+          echo "<td>".$j*$i."</td>";
+        echo "</tr>";
+        $j++;
+      }
+    echo "</table>";
+    $i++;
+  } while ($i <= 10);
+?>
